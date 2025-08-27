fb3f596adc4b0f09b016ba1964a3a0f32b0929e0
pongkrit12 <pongkrit.b@ku.th>
2025-08-27 23:00:49 +0700

    finish v.1

--- src/test/java/ku/cs/models/StudentListTest.java
+++ src/test/java/ku/cs/models/StudentListTest.java
@@ -3,6 +3,7 @@ package ku.cs.models;
 import ku.cs.services.StudentHardCodeDatasource;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -13,19 +14,49 @@ class StudentListTest {
     private static StudentList studentList;
 
     @BeforeAll
-    static void initial(){
+    static void initial() {
         StudentHardCodeDatasource datasource = new StudentHardCodeDatasource();
         studentList = datasource.readData();
     }
+
     @Test
-    void testFindStudentById(){
-        Student target = new Student("6710400001", "First");
-        assertEquals(target,studentList.findStudentById("6710400001"));
+    @DisplayName("test add myself")
+    void testAddNewStudent() {
+        studentList.addNewStudent("6710451062","pongkrit");
+        assertEquals("6710451062", studentList.findStudentById("6710451062").getId());
+        assertEquals("pongkrit", studentList.findStudentById("6710451062").getName());
     }
 
     @Test
-    void testGiveScoreToId(){
-        studentList.giveScoreToId("6710400001",15);
-        assertEquals(15,studentList.findStudentById("6710400001").getScore());
+    @DisplayName("test filter ir")
+    void testFilterByName() {
+        assertEquals(2, studentList.filterByName("ir").getStudents().size());
+    }
+
+    @Test
+    @DisplayName("test find studentbyid")
+    void testFindStudentById() {
+        assertEquals("6710400001", studentList.findStudentById("6710400001").getId());
+    }
+
+    @Test
+    @DisplayName("test give score")
+    void testGiveScoreToId() {
+        studentList.giveScoreToId("6710400001", 15);
+        assertEquals(15, studentList.findStudentById("6710400001").getScore());
+    }
+
+    @Test
+    @DisplayName("test view grade")
+    void testViewGradeOfId() {
+        assertEquals("F", studentList.findStudentById("6710400001").getGrade());
+    }
+
+
+    @Test
+    @DisplayName("test get student")
+    void testGetStudent() {
+        assertEquals("6710400001", studentList.findStudentById("6710400001").getId());
+        assertEquals("First", studentList.findStudentById("6710400001").getName());
     }
 }
